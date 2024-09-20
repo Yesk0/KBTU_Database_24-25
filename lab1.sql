d59cdb7323d1a745784ff6b72e79f88f1b905a8b
Yesko <iskoaleksandor@gmail.com>
2024-09-21 02:24:15 +0500

    ...

---
+++ lab1.sql
@@ -0,0 +1,26 @@
+
+CREATE DATABASE Faculty;
+
+CREATE TABLE Students (
+    id SERIAL,
+    firstname VARCHAR(70),
+    lastname VARCHAR(70)
+);
+
+/*ALTER TABLE Students ADD gender INTEGER;
+
+ALTER TABLE Students ALTER COLUMN gender SET DEFAULT 0;
+
+ALTER TABLE Students ADD PRIMARY KEY (id);
+
+CREATE TABLE tasks (
+    id SERIAL,
+    name VARCHAR(50),
+    user_id INTEGER
+);
+
+DROP TABLE tasks;
+
+DROP TABLE Students;
+
+DROP DATABASE Faculty;*/
